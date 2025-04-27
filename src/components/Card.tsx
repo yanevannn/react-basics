0f22932f85abf0bb13fe16e83bc43518184e4c40
yanevannn <yanevan03@gmail.com>
2025-04-28 03:04:36 +0800

    Update: styling component using css file

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,7 +1,7 @@
 const Card = () => {
   return (
     <>
-    <div style={{border:"1px solid red", borderRadius:"10px", padding:"10px", display:"inline-block" }}>
+    <div className="card">
       <h4>My Names John</h4>
       <p>20 year</p>
       <p> from USA</p>
