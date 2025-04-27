3311e13dbdda4c501627218434277a3c68cb5782
yanevannn <yanevan03@gmail.com>
2025-04-28 02:45:01 +0800

    Update : create basic components on react using arrow function

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Header from "./components/Header";
 import Welcome from "./components/Welcome";
+import Card from "./components/Card";
 
 function App() {
   return (
     <>
       <Header />
       <Welcome />
+      <Card />
     </>
   );
 }
---
+++ src/components/Card.tsx
@@ -0,0 +1,11 @@
+const Card = () => {
+  return (
+    <>
+      <h4>My Names John</h4>
+      <p>20 year</p>
+      <p> from USA</p>
+    </>
+  );
+};
+
+export default Card;
